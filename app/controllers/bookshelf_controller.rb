8be6fd25f103534f5cf9d85a8f39bd5785dcfb77
shonoru <shonoru@gmail.com>
2011-10-24 14:40:34 +0800

    real url

--- app/controllers/bookshelf_controller.rb
+++ app/controllers/bookshelf_controller.rb
@@ -3,8 +3,8 @@ class BookshelfController < ApplicationController
   require 'open-uri'
   
   def index
-    # @doc = Nokogiri::HTML(open('http://shonoru.tadalist.com/lists/2006813.rss?token=11547c864e558a610908b7ac65e2f52f'))
-    @doc = Nokogiri::XML(open("http://0.0.0.0:3000/list.xml"))
+    @doc = Nokogiri::HTML(open('http://shonoru.tadalist.com/lists/2006813.rss?token=11547c864e558a610908b7ac65e2f52f'))
+    # @doc = Nokogiri::XML(open("http://0.0.0.0:3000/list.xml"))
     @book_titles = @doc.xpath('//item/title')
   end
 
